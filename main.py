5e88f2ae5b4381e0e8357608e47840b225b23576
vinikunrath <topfps28@gmail.com>
2023-11-03 16:39:58 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+
+menor = min(num1, num2, num3)
+
+print(f"O menor número é {menor}.")
